614dffb4d6297e1d66c3a0e3fde6f3aaff9f1745
duchess <duchessmein@gmail.com>
2023-08-08 11:27:27 -0700

    password saved in localstorage

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-document
-  .getElementById("passwordForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // prevent the default form submission behavior
+// Retrieve the flag indicating whether the password has been entered before
+const hasEnteredPassword = localStorage.getItem("hasEnteredPassword");
 
-    // define the correct password to access the content
-    const correctPassword = "orb";
+if (hasEnteredPassword) {
+  showContent();
+} else {
+  document
+    .getElementById("passwordForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    // retrieve the password entered by the user
-    const enteredPassword = document.getElementById("password").value;
+      const correctPassword = "orb2";
+      const enteredPassword = document.getElementById("password").value;
 
-    // compare the entered password with the correct password
-    if (enteredPassword === correctPassword) {
-      // password is correct, show the protected content
-      document.getElementById("passwordForm").style.display = "none";
-      document.getElementById("content").style.display = "block";
-    } else {
-      // password is incorrect, display an error message
-      alert("Incorrect password. Please try again.");
-    }
-  });
+      if (enteredPassword === correctPassword) {
+        localStorage.setItem("hasEnteredPassword", true);
+        showContent();
+      } else {
+        alert("Incorrect password. Please try again.");
+      }
+    });
+}
+
+function showContent() {
+  document.getElementById("passwordForm").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
