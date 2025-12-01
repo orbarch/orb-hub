42e0d827259c8e772caea5ba00f4a3c5b4e162ea
duchess <duchessmein@gmail.com>
2025-12-01 10:42:30 -0700

    added lunch menus

--- src/data/emresources.ts
+++ src/data/emresources.ts
@@ -19,6 +19,13 @@ export const emResources: StandardSection[] = [
         status: "approved",
         lastUpdated: "2025-12-02",
       },
+      {
+        title: "Lunch Contact & Menus",
+        description: "",
+        link: "https://docs.google.com/spreadsheets/d/1svj5DU8Xz979jPeLOqIUGoeySb1Nftgl/edit?usp=sharing&ouid=115917362032696382498&rtpof=true&sd=true",
+        status: "approved",
+        lastUpdated: "2025-12-02",
+      },
     ],
   },
 ];
